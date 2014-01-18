e6315aee43d57762460ecf152dcb6d7bda1cf0b8
Russell Ahlstrom <russell.ahlstrom@gmail.com>
2014-01-18 00:31:25 -0700

    Add buttons css and example to styleguide

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="css/layout.css" />
     <link rel="stylesheet" href="css/styleguide.css" />
     <link rel="stylesheet" href="css/alerts.css" />
+    <link rel="stylesheet" href="css/buttons.css" />
 </head>
 <body>
 
--- public/index.php
+++ public/index.php
@@ -6,8 +6,12 @@ $kss = new \Scan\Kss\Parser('\css');
 
 require_once('../includes/header.php');
 
+$section = $kss->getSection('2.1');
+include('../includes/block.php');
+
 $section = $kss->getSection('1.1');
 include('../includes/block.php');
+
 $section = $kss->getSection('1.2');
 include('../includes/block.php');
 
